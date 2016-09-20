94cf81183ae353781dc661daf07da8a7fccd9f90
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2016-09-20 11:48:09 +0200

    Moved shuffle function to header

--- sp.cpp
+++ sp.cpp
@@ -1,23 +1,5 @@
 #include "lpisg.h"
 
-// Shuffle the content of an array
-
-void shuffle(void *array, size_t n, size_t size) {
-
-	uint8_t tmp[size];
-	uint8_t *arr = (uint8_t *)array;
-
-	if (n > 1) {
-		for (size_t i = 0; i < n - 1; ++i) {
-			size_t rnd = (size_t) rand();
-			size_t j = i + rnd / (RAND_MAX / (n - i) + 1);
-			memcpy(tmp, arr + j * size, size);
-			memcpy(arr + j * size, arr + i * size, size);
-			memcpy(arr + i * size, tmp, size);
-		}
-	}
-}
-
 // A* sub-rountines
 
 __attribute__((always_inline)) inline
