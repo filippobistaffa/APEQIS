646f87d2dee82293a4b00f77a41ef965829a28c1
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2016-10-08 10:08:44 +0200

    Do not redefine printbuf

--- apelib.h
+++ apelib.h
@@ -4,6 +4,9 @@
 #define LTL(X, Y) (GET(l, *(X)) == GET(l, *(Y)) ? (*(X)) < (*(Y)) : GET(l, *(X)) > GET(l, *(Y)))
 #define LEL(X, Y) (GET(l, *(X)) == GET(l, *(Y)) ? (*(X)) <= (*(Y)) : GET(l, *(X)) > GET(l, *(Y)))
 
+#ifndef PRINTBUF
+#define PRINTBUF
+
 #include <iostream>
 template <typename type>
 __attribute__((always_inline)) inline
@@ -14,6 +17,8 @@ void printbuf(const type *buf, unsigned n, const char *name = NULL) {
 	while (n--) std::cout << *(buf++) << " ";
 	printf("]\n");
 }
+
+#endif
 
 double *apeqis(const edge *g, value (*cf)(agent *, const chunk *, void *), void *data = NULL,
 	       const chunk *l = NULL, agent maxc = N, agent maxl = N);
