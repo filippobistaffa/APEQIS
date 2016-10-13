47157b89c74aa147b2d23407539847cb46e0aa9c
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2016-10-13 11:06:49 +0200

    Copy data

--- cgls.cu
+++ cgls.cu
@@ -13,8 +13,17 @@ unsigned cudacgls(const value *val, const unsigned *ptr, const unsigned *ind, co
 	cudaMallocManaged(&d_b, sizeof(value) * m);
 	cudaMallocManaged(&d_x, sizeof(value) * n);
 
+	memcpy(d_val, val, sizeof(value) * nnz);
+	memcpy(d_ptr, ptr, sizeof(int) * n + 1);
+	memcpy(d_ind, ind, sizeof(int) * nnz);
+	memcpy(d_b, b, sizeof(value) * m);
+	memset(d_x, 0, sizeof(value) * n);
+
 	unsigned rc = cgls::Solve<value, cgls::CSC>(d_val, d_ptr, d_ind, (int)m, (int)n, (int)nnz,
 						    d_b, d_x, 0, TOLERANCE, MAXITERATIONS, !CGLSDEBUG);
+
+	cudaDeviceSynchronize();
+	memcpy(x, d_x, sizeof(value) * n);
 
 	cudaFree(d_val);
 	cudaFree(d_ptr);
