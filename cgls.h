3efa99e3f815ad58d1b54bfc600deeaab5973138
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2017-02-06 13:59:44 +0100

    CUDA error check macro

--- cgls.h
+++ cgls.h
@@ -8,6 +8,10 @@
 #define TOLERANCE 1e-4
 #define MAXITERATIONS 500
 
+#define CUDACHECKERROR() { \
+cudaError_t e=cudaGetLastError(); \
+if (e!=cudaSuccess) { printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e)); exit(0); } }
+
 unsigned cudacgls(const value *val, const unsigned *ptr, const unsigned *ind, unsigned m,
 		  unsigned n, unsigned nnz, value *b, value *x, float *rt, bool quiet);
 
